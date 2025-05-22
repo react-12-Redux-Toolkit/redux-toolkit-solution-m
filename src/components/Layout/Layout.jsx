8adf5513a90568ac165eab92114aa6942703dc08
mplesha <mykhaylo.plesha@gmail.com>
2025-05-22 07:27:22 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,18 +1,13 @@
 import { Fragment } from 'react';
-//import PropTypes from 'prop-types';
 import MainHeader from './MainHeader';
 
 const Layout = (props) => {
   return (
-    <Fragment>
+    <>
       <MainHeader />
       <main>{props.children}</main>
-    </Fragment>
+    </>
   );
 };
 
-/*Layout.propTypes = {
-  children: PropTypes.node.isRequired
-};
-*/
 export default Layout;
